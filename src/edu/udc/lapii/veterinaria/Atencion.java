ad40a44551a0f1817943e6fcd88f45fc5463164a
moises <moiair@gmail.com>
2016-04-21 14:46:20 -0300

    se creo una clase de Atencion

---
+++ src/edu/udc/lapii/veterinaria/Atencion.java
@@ -0,0 +1,18 @@
+package edu.udc.lapii.veterinaria;
+
+import java.util.Date;
+
+public class Atencion {
+	
+	private Date fechaDeAtencion;
+	private String dianostico;
+	private String observaciones;
+	
+	private Ambulatorio ambulatorio;
+	private Domiciliaria domiciliaria;
+	private Internacion internacion;
+	private PracticaMedica practicaMedica;
+	private Veterinario veterinario;
+
+
+}
