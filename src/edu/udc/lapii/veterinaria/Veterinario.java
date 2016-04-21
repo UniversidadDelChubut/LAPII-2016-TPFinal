9bf42e69bbd9acf83a1505b627d1fb7c26bed9d5
MatiasUDC <mabruzzo@udc.edu.ar>
2016-04-21 15:48:07 -0300

    Update Veterinario.java

--- src/edu/udc/lapii/veterinaria/Veterinario.java
+++ src/edu/udc/lapii/veterinaria/Veterinario.java
@@ -1,6 +1,37 @@
 package edu.udc.lapii.veterinaria;
 
 public class Veterinario {
+	private String Nombre;
+	private String Apellido;
+	private int Matricula;
+	private int NCelular;
+	
+	
+	public Veterinario(String nombre, String apellido, int matricula, int nCelular) {
+		setNombre(nombre);
+		setApellido(apellido);
+		setMatricula(matricula);
+		setNCelular(nCelular);
+	}
 
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+
+
+	public void setMatricula(int matricula) {
+		Matricula = matricula;
+	}
+
+
+	public void setNCelular(int nCelular) {
+		NCelular = nCelular;
+	}
 	
 }
