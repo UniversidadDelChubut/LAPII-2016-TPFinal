98e85cfc7244b021f3e0bbf7bfb0636be3fa0868
moises <moiair@gmail.com>
2016-04-21 16:23:27 -0300

    cambios

--- src/edu/udc/lapii/veterinaria/Atencion.java
+++ src/edu/udc/lapii/veterinaria/Atencion.java
@@ -13,5 +13,54 @@ public class Atencion {
 	private Internacion internacion;
 	private PracticaMedica practicaMedica;
 	private Veterinario veterinario;
+	
+	public Atencion(Date fechaDeAtencion, String dianostico, String observaciones, Ambulatorio ambulatorio,
+			Domiciliaria domiciliaria, Internacion internacion, PracticaMedica practicaMedica,
+			Veterinario veterinario) {
+		super();
+		this.fechaDeAtencion = fechaDeAtencion;
+		this.dianostico = dianostico;
+		this.observaciones = observaciones;
+		this.ambulatorio = ambulatorio;
+		this.domiciliaria = domiciliaria;
+		this.internacion = internacion;
+		this.practicaMedica = practicaMedica;
+		this.veterinario = veterinario;
+	}
 
+	public Date getFechaDeAtencion() {
+		return fechaDeAtencion;
+	}
+
+	public String getDianostico() {
+		return dianostico;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public Ambulatorio getAmbulatorio() {
+		return ambulatorio;
+	}
+
+	public Domiciliaria getDomiciliaria() {
+		return domiciliaria;
+	}
+
+	public Internacion getInternacion() {
+		return internacion;
+	}
+
+	public PracticaMedica getPracticaMedica() {
+		return practicaMedica;
+	}
+
+	public Veterinario getVeterinario() {
+		return veterinario;
+	}
+	
+	
+	
+	
 }
