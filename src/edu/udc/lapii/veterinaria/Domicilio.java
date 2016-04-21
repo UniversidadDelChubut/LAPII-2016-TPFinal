a7a6515a48a75fccf4d66e234a24f0665932c68f
Cesar Matias <cesar@cesar-MS-7309>
2016-04-21 19:08:10 -0300

    Se añade Localidad y Domicilio

---
+++ src/edu/udc/lapii/veterinaria/Domicilio.java
@@ -0,0 +1,21 @@
+package edu.udc.lapii.veterinaria;
+
+public class Domicilio {
+	
+	private String calle;
+	private int numeroCasa;
+	
+	public String getCalle() {
+		return calle;
+	}
+	public int getNumeroCasa() {
+		return numeroCasa;
+	}
+	
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public void setNumeroCasa(int numeroCasa) {
+		this.numeroCasa = numeroCasa;
+	}
+}
---
+++ src/edu/udc/lapii/veterinaria/Localidad.java
@@ -0,0 +1,14 @@
+package edu.udc.lapii.veterinaria;
+
+public class Localidad {
+	
+	private String nombre;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
