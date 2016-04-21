83f7b438e95b70620a2e7d1f7352761b287c5951
Santiago Della Torre <sdellatorre@gmail.com>
2016-04-21 14:34:15 -0300

    Agrego Clases

--- src/edu/udc/lapii/veterinaria/Internacion.java
+++ src/edu/udc/lapii/veterinaria/Internacion.java
@@ -4,6 +4,16 @@ import java.util.Date;
 
 public class Internacion extends Atencion {
 	
+	public Internacion(Date fechaDeAtencion, String dianostico, String observaciones, Ambulatorio ambulatorio,
+			Domiciliaria domiciliaria, Internacion internacion, PracticaMedica practicaMedica, Veterinario veterinario,
+			Vacunacion vacunacion) {
+		super(fechaDeAtencion, dianostico, observaciones, ambulatorio, domiciliaria, internacion, practicaMedica, veterinario,
+				vacunacion);
+		// TODO Auto-generated constructor stub
+	}
+
 	private Date fechaDeAlta;
 
+	
+	
 }
