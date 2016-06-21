d2e7aa1324e9f2251e49c78ec6b662d6015bae24
cmcuriqueo <cmcuriqueo@udc.edu.ar>
2016-06-21 16:53:01 -0300

    Agrego Clases HistoriaClinica y Registro

---
+++ src/edu/udc/lapii/veterinaria/HistoriaClinica.java
@@ -0,0 +1,23 @@
+package edu.udc.lapii.veterinaria;
+
+import java.util.LinkedList;
+
+public class HistoriaClinica {
+	String nombre;
+	String matricula;
+	LinkedList<Registro> registro = null;
+	
+	public HistoriaClinica( String nombre, String matricula) {
+		this.matricula = matricula;
+		this.nombre = nombre;
+		this.registro = new LinkedList<Registro>();
+	}
+	
+	public void setRegistro(Registro registro) {
+		this.registro.add(registro);
+	}
+
+	public LinkedList<Registro> getRegistro() {
+		return this.registro;
+	}
+}
