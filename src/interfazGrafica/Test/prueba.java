decc7a89ae7d0d7b1967e45bd854060784c87dcf
moises <moiair@gmail.com>
2016-06-28 19:32:11 -0300

    da

---
+++ src/interfazGrafica/Test/prueba.java
@@ -0,0 +1,16 @@
+package interfazGrafica.Test;
+
+import interfazGrafica.HistoriaClinica.*;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		datosRecibidosHClinica nuevo = new datosRecibidosHClinica();
+		
+		System.out.println("******"+nuevo.MostrarAnimal().getMatricula());
+		//System.out.println("ÇÇÇÇÇÇÇÇÇÇÇ"+ datosRecibidosHClinica.MostrarAnimal().getMatricula());
+
+	}
+
+}
